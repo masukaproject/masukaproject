256646c735da1df510d8f9a161aaefe7ac5c20e0
Uthman Mohamed <83053931+1239uth@users.noreply.github.com>
2021-07-25 22:36:46 -0400

    project start

---
+++ MasukaApp/MasukaAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MasukaAppApp.swift
+//  MasukaApp
+//
+//  Created by Uthman Mohamed on 2021-07-25.
+//
+
+import SwiftUI
+
+@main
+struct MasukaAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
