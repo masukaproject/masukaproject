1e17a772adcaa22182209568ff2ac5be227864d5
Abdullah Shahid <abdullaaaah_@outlook.com>
2021-07-28 18:02:57 -0400

    added update acc page

---
+++ MasukaApp/Views/Sign-In/SignUp2View.swift
@@ -0,0 +1,20 @@
+//
+//  SignUp2View.swift
+//  MasukaApp
+//
+//  Created by Abdullah Shahid on 2021-07-28.
+//
+
+import SwiftUI
+
+struct SignUp2View: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SignUp2View_Previews: PreviewProvider {
+    static var previews: some View {
+        SignUp2View()
+    }
+}
