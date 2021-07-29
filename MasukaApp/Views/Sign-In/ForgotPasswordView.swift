f45f2c4cc5b2fb2e52dc9a499ce28930541e2003
sarinlover12 <86638086+sarinlover12@users.noreply.github.com>
2021-07-28 22:52:26 -0400

    Start ForgotPass Page
    Initial text and button element additions.

--- MasukaApp/Views/Sign-In/ForgotPasswordView.swift
+++ MasukaApp/Views/Sign-In/ForgotPasswordView.swift
@@ -9,9 +9,33 @@ import SwiftUI
 
 struct ForgotPasswordView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+        
+        VStack {
+            
+            Text("Password Reset").bold().font(.system(size: 20, weight: .medium))
+            
+            Text("Forgot Password").bold().font(.system(size: 32, weight: .medium)).offset(y: 150)
+            
+            Text("Enter your email address or username and we will send you a link to get back to your account.").font(.system(size: 17, weight: .medium)).offset(y: 200).padding(.horizontal, 40).multilineTextAlignment(.center)
+        
+        Button(action: {}) {
+            Text("Continue")
+                .foregroundColor(.white)
+                .font(.system(size: 24, weight: .medium))
+        }.frame(maxWidth: .infinity)
+        .padding(.vertical, 20)
+        .background(Color(.sRGB, red: 0.32, green: 0.15, blue: 0.09).opacity(1))
+        .cornerRadius(20)
+        .padding(.horizontal, 40)
+        .offset(y: 600)
+        Spacer()
+            
+        }
+   }
+    
 }
+
+
 
 struct ForgotPasswordView_Previews: PreviewProvider {
     static var previews: some View {
--- MasukaApp/Views/Sign-In/SignUpView.swift
+++ MasukaApp/Views/Sign-In/SignUpView.swift
@@ -1,3 +1,4 @@
+
 //
 //  RegisterView.swift
 //  MasukaApp
